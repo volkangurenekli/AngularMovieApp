4806ce8909d16c264427411fbf5eb7df2923fd76
VOLKAN GÜRENEKLİ <59292167+volkangurenekli@users.noreply.github.com>
2021-11-08 11:50:46 +0300

    first

---
+++ README.md
@@ -0,0 +1,3 @@
+yarn prepare
+
+yarn start
---
+++ src/app/app.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { CategoryComponent } from './category/category.component';
+import { MoviesComponent } from './movies/movies.component';
+import { MovieComponent } from './movies/movie/movie.component';
+import { MovieDetailsComponent } from './movie-details/movie-details.component';
+import { FooterComponent } from './footer/footer.component';
+import { SummaryPipe } from './pipes/summary.pipe';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MovieFilterPipe } from './pipes/movie-filter.pipe';
+import { AlertifyService } from './services/alertify.service';
+import { AppRoutingModule } from './app-routing.module';
+import { MovieCreateComponent } from './movie-create/movie-create.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    CategoryComponent,
+    MoviesComponent,
+    MovieComponent,
+    MovieDetailsComponent,
+    FooterComponent,
+    SummaryPipe,
+    MovieFilterPipe,
+    MovieCreateComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    AppRoutingModule,
+    ReactiveFormsModule,
+  ],
+  providers: [AlertifyService],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
